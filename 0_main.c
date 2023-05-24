ccfd8efc44a63d2d72f6bdd66f1e6f82dfea5af9
ericmuthoka <muthokaerick@gmail.com>
2023-05-24 04:51:40 -0700

    PATH

--- 0_main.c
+++ 0_main.c
@@ -1,24 +1,60 @@
 #include "main.h"
 /**
  * main - Entry point
+ * @argc: arguments count
+ * @argv: arguments vector
+ * @env: environment
  *
  * Return: Always0
  */
 
-int main(void)
+int main(__attribute__((unused)) int argc,
+		__attribute__((unused)) char **argv,
+		char **env)
 {
 	char *line = NULL;
 	char **buff;
+	unsigned int i = 0;
+	char file_path[MAX_PATH_LENGTH];
+	char* path;
+	char* dir;
+
 
 	while (1)
 	{
-		printf("$ ");
+		putchar('$');
 		line = get_line();
 		buff = parse_input(line);
-		create_process(buff);
+		if (strcmp(buff[0], "exit") == 0)
+		{
+			break;
+		}
+		else if (strcmp(buff[0], "env") == 0)
+		{
+			while (env[i] != NULL)
+			{
+				printf("%s\n", env[i]);
+				i++;
+			}
+		}
+		else
+		{
+			create_process(buff);
+		}
 
 		free(buff);
 		free(line);
+		path = getenv("PATH");
+		dir = strtok(path, ":");
+
+		while (dir != NULL) {
+
+			strcpy(file_path, dir);
+			strcat(file_path, "/");
+			strcat(file_path, line);
+
+		}
 	}
 	return (0);
 }
+
