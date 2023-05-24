6dab071aeb5de3669d06be708b1431d122366e64
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-05-24 23:18:52 +0000

    simple shell

--- getline.c
+++ getline.c
@@ -10,12 +10,19 @@ char *get_line()
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t n_char;
+	int i = 0;
 
 	n_char = getline(&line, &len, stdin);
 	if (n_char == -1)
 	{
 		free(line);
 		exit(EXIT_FAILURE);
+	}
+	while (line[i])
+	{
+		if (line[i] == '\n')
+			line[i] = '\0';
+		i++;
 	}
 	return (line);
 }
