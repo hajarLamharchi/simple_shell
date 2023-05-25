6be0b95528f56643a72977cda318115dce2a838a
ericmuthoka <muthokaerick@gmail.com>
2023-05-25 05:04:36 -0700

    PATH

--- 1_PATH.c
+++ 1_PATH.c
@@ -1,12 +1,14 @@
 #include "main.h"
-
+/**
+ * path - function that handles path
+ * Return: void
+ */
 int path(void)
 {
 	char file_path[MAX_PATH_LENGTH];
 	char *path;
 	char *dir;
 	char *line = NULL;
-	int command_exists = 0;
 
 	path = getenv("PATH");
 	line = get_line();
@@ -21,28 +23,10 @@ int path(void)
 			strcat(file_path, "/");
 			strcat(file_path, line);
 
-			if (access(file_path, F_OK) == 0)
-			{
-				command_exists = 1;
-				break;
-			}
-
 			dir = strtok(NULL, ":");
 		}
 
 		free(line);
-
-		if (command_exists)
-		{
-			break;
-		}
 	}
-
-	if (!command_exists)
-	{
-		printf("Command does not exist.\n");
-	}
-
-	return 0;
+	return (0);
 }
-
